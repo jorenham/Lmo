82a536b131878bb7b60f9dedb05b41ecfc23085d
jorenham <jhammudoglu@gmail.com>
2023-05-19 03:38:59 +0200

    added `dtype` parameter to `lmo.weights.{}_weights`

--- lmo/weights.py
+++ lmo/weights.py
@@ -1,6 +1,7 @@
 __all__ = 'tl_weights', 'l_weights'
 
 from math import comb, fsum
+from typing import TypeVar
 
 import numpy as np
 import numpy.typing as npt
@@ -9,11 +10,21 @@ from ._utils import expand_trimming
 from .typing import Trimming
 
 
-def tl_weights(n: int, r: int, /, trim: Trimming) -> npt.NDArray[np.float_]:
-    """
-    Linear sample weights for calculation of the r-th TL(s, t)-moment.
+_T = TypeVar('_T', bound=npt.NBitBase)
 
-    Args:
+
+def tl_weights(
+    n: int,
+    r: int,
+    /,
+    trim: Trimming = 1,
+    *,
+    dtype: type[np.floating[_T]] = np.float_,
+) -> npt.NDArray[np.floating[_T]]:
+    """
+    Linear sample weights for calculation of the r-th TL-moment.
+
+    Parameters:
         n: Sample size.
         r: L-moment order, e.g. 1 for location, and 2 for scale.
         trim:
@@ -23,12 +34,29 @@ def tl_weights(n: int, r: int, /, trim: Trimming) -> npt.NDArray[np.float_]:
             - `t: int`, or `(t: int)` as alias for `(t, t)`, or
             - `()` as alias for `(0, 0)`.
 
+            If not provided, `1` will be used by default.
+
+    Other parameters:
+        dtype:
+            Desired output floating type for the weights, e.g,
+            `numpy.float128`. Default is `numpy.float64`.
+            Must be a (strict) subclass of `numpy.floating`.
+
     Returns:
-        w_j:
+        w_r:
             A vector of size `n`, with linear weights for each of the
             (ordered) samples.
 
     """
+
+    if not issubclass(
+        dtype,
+        np.floating
+    ):  # pyright: ignore [reportUnnecessaryIsInstance]
+        raise TypeError(
+            f'dtype must be a subclass of numpy.floating, got {dtype!r}'
+        )
+
     if r <= 0:
         raise ValueError(f'expected r > 0, got {r} <= 0')
 
@@ -39,24 +67,30 @@ def tl_weights(n: int, r: int, /, trim: Trimming) -> npt.NDArray[np.float_]:
 
     # pre-calculate the terms that are independent on j
     m = r * comb(n, r + tl + tr)
-    w_k = np.empty(r)
+    w_k = np.empty(r, dtype=dtype)
     for k in range(r):
         w_k[k] = (-1) ** k * comb(r - 1, k) / m
 
     # sample weights
-    w_j = np.zeros(n)
+    w_r = np.zeros(n, dtype=dtype)
     for j in range(tl, n - tr):
         # divide inside the loop, to avoid overflows
-        w_j[j] = fsum(
+        w_r[j] = fsum(
             comb(j, r + tl - k - 1) * comb(n - j - 1, tr + k) * w_k[k]
             for k in range(r)
         )
 
-    return w_j
+    return w_r
 
 
-def l_weights(n: int, r: int, /) -> npt.NDArray[np.float_]:
+def l_weights(
+    n: int,
+    r: int,
+    /,
+    *,
+    dtype: type[np.floating[_T]] = np.float_,
+) -> npt.NDArray[np.floating[_T]]:
     """
-    Alias for `tl_weights(n, r, 0, 0)`.
+    Alias for [`tl_weights(n, r, 0)`][lmo.weights.tl_weights].
     """
-    return tl_weights(n, r, 0)
+    return tl_weights(n, r, 0, dtype=dtype)
