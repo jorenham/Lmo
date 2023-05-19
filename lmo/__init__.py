2e97569b20fe825245fe82a7dea0a36a4e0fd140
jorenham <jhammudoglu@gmail.com>
2023-05-19 18:10:31 +0200

    removed debug comment

--- lmo/__init__.py
+++ lmo/__init__.py
@@ -42,30 +42,3 @@ from .multivariate import *  # noqa
 
 
 __version__: _Final[str] = _get_version()
-
-"""
-l_moment
-l_ratio
-l_loc
-l_scale
-l_skew
-l_kurt
-tl_moment
-tl_ratio
-tl_loc
-tl_scale
-tl_skew
-tl_kurt
-l_comoment
-l_coratio
-l_coloc
-l_coscale
-l_coskew
-l_cokurt
-tl_comoment
-tl_coratio
-tl_coloc
-tl_coscale
-tl_coskew
-tl_cokurt
-"""
