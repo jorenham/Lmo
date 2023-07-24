c217d91ef9acb4de3c145e79c1aa7d924ca99746
jorenham <jhammudoglu@gmail.com>
2023-07-24 04:06:10 +0200

    flaky doctest fix

--- lmo/linalg.py
+++ lmo/linalg.py
@@ -102,7 +102,7 @@ def pascal(
                [-1,  1,  0,  0],
                [ 1, -2,  1,  0],
                [-1,  3, -3,  1]])
-        >>> np.linalg.inv(pascal(4)).astype(np.int_)
+        >>> np.rint(np.linalg.inv(pascal(4))).astype(np.int_)
         array([[ 1,  0,  0,  0],
                [-1,  1,  0,  0],
                [ 1, -2,  1,  0],
