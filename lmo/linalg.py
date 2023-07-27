various numerical stability fixes

lmo/linalg.py
@@ -23,9 +23,9 @@ else:
 import numpy as np
 import numpy.typing as npt
 
-from .typing import AnyInt
+from .typing import AnyFloat, AnyInt
 
-T = TypeVar('T', bound=np.integer[Any] | np.floating[Any] | np.object_)
+T = TypeVar('T', bound=np.object_ | np.integer[Any] | np.floating[Any])
 
 
 def sandwich(
@@ -128,20 +128,31 @@ def pascal(
         out[1::2, 0] = -1
 
     jj = np.arange(1, k, dtype=np.int_)
-    for i in range(1, k):
+    for i in jj:
         out[i, 1:i+1] = i * out[i - 1, :i] // jj[:i]
 
     return out
 
 
-def ir_pascal(k: int, /) -> npt.NDArray[np.float_]:
+def ir_pascal(
+    k: int,
+    /,
+    dtype: np.dtype[T] | type[T] = np.float_,
+) -> npt.NDArray[np.float_]:
     r"""
     Inverse regulatized lower-diagonal Pascal matrix,
     $\bar{L}_{ij} = L^{-1}_ij / i$.
 
     Used to linearly combine order statistics order statistics into L-moments.
     """
-    return pascal(k, np.float_, inv=True) / np.arange(1, k + 1)[:, None]
+    # use native ints to reduce the effect of over-/underflows
+    dtype_native = k > 62
+    _dtype = np.object_ if dtype_native else np.int_
+
+    p = pascal(k, dtype=_dtype, inv=True)
+    out = p / np.arange(1, k + 1, dtype=_dtype)[:, None]  # type: ignore
+
+    return np.asarray(out, dtype)
 
 
 def sh_legendre(
@@ -238,8 +249,8 @@ def _sh_jacobi_f(
 
 def sh_jacobi(
     k: AnyInt,
-    a: T | int,
-    b: T | int,
+    a: AnyFloat,
+    b: AnyFloat,
     /,
     dtype: np.dtype[T] | type[T] | None = None,
 ) -> npt.NDArray[T | np.int_]:
@@ -303,10 +314,10 @@ def sh_jacobi(
         raise ValueError(msg)
 
     _dtype = dtype or np.asarray([a, b]).dtype.type
-    if np.issubdtype(_dtype, np.integer):
-        return _sh_jacobi_i(int(k), int(a), int(b), _dtype)
+    if np.issubdtype(_dtype, np.integer) or np.issubdtype(_dtype, np.bool_):
+        return _sh_jacobi_i(int(k), int(a), int(b), dtype=_dtype)
 
-    return _sh_jacobi_f(int(k), float(a), float(b), _dtype)
+    return _sh_jacobi_f(int(k), float(a), float(b), dtype=_dtype)
 
 
 
@@ -401,7 +412,7 @@ def trim_matrix(
     if r == 0:
         return np.empty((0, 0), dtype=dtype)
 
-    rr = np.linspace(1, r, r, dtype=dtype)
+    rr = np.arange(1, r + 1, dtype=np.int_)
 
     t1, t2 = trim
     nc = t1 + t2 - 1 + 2 * rr