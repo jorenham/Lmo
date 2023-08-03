5d966072b730a71c21f0a971208aa481920e91fb
jorenham <jhammudoglu@gmail.com>
2023-08-03 23:10:57 +0200

    typing protocol for numpy.polynomial types

--- lmo/typing.py
+++ lmo/typing.py
@@ -1,6 +1,9 @@
+# ruff: noqa: D102,D105,D107
+
 """Numpy-related type aliasses for internal use."""
 
 __all__ = (
+    'AnyScalar',
     'AnyNDArray',
 
     'AnyBool',
@@ -18,18 +21,24 @@ __all__ = (
     'SortKind',
     'IndexOrder',
 
+    'PolySeries',
+
     'LMomentOptions',
     'LComomentOptions',
 )
 
-from collections.abc import Sequence
+from collections.abc import Iterator, Sequence
 from typing import (
     Any,
+    ClassVar,
     Literal,
     Protocol,
+    SupportsInt,
     TypeAlias,
+    TypeGuard,
     TypeVar,
     TypedDict,
+    overload,
     runtime_checkable,
 )
 
@@ -45,28 +54,252 @@ class _SupportsArray(Protocol[T_co]):
 
 
 # scalar types
-_NumpyBool: TypeAlias = np.bool_
-_NumpyInteger: TypeAlias = np.integer[Any] | _NumpyBool
-_NumpyFloating: TypeAlias = np.floating[Any] | _NumpyInteger
+_NpBool: TypeAlias = np.bool_
+_NpInt: TypeAlias = np.integer[Any]
+_NpFloat: TypeAlias = np.floating[Any]
+_NpComplex: TypeAlias = np.complexfloating[Any, Any]
+_NpNumber: TypeAlias = np.number[Any] | _NpBool
+_NpScalar: TypeAlias = np.generic
 
-AnyBool: TypeAlias = _NumpyBool | bool
-AnyInt: TypeAlias = _NumpyInteger | int | bool
-AnyFloat: TypeAlias = _NumpyFloating | float | int | bool
+AnyBool: TypeAlias = bool | _NpBool
+AnyInt: TypeAlias = int | _NpInt | _NpBool
+AnyFloat: TypeAlias = float | _NpFloat | AnyInt
+AnyComplex: TypeAlias = complex | _NpComplex | AnyFloat  # no float
+AnyNumber: TypeAlias = int | float | complex | _NpNumber
+AnyScalar: TypeAlias = int | float | complex | str | bytes | _NpScalar
 
 # array-like flavours (still waiting on numpy's shape typing)
+# - `{}Vector`: ndim == 1
+# - `{}Matrix`: ndim == 2
+# - `{}Tensor`: ndim >= 3
 AnyNDArray: TypeAlias = npt.NDArray[T] | _SupportsArray[T]
 
-IntVector: TypeAlias = AnyNDArray[_NumpyInteger] | Sequence[AnyInt]
-IntMatrix: TypeAlias = AnyNDArray[_NumpyInteger] | Sequence[IntVector]
-IntTensor: TypeAlias = AnyNDArray[_NumpyInteger] | Sequence['IntTensor']
+_ArrayZ: TypeAlias = AnyNDArray[_NpInt] | AnyNDArray[_NpBool]
+IntVector: TypeAlias = _ArrayZ | Sequence[AnyInt]
+IntMatrix: TypeAlias = _ArrayZ | Sequence[Sequence[AnyInt]]
+IntTensor: TypeAlias = _ArrayZ | Sequence[IntMatrix | 'IntTensor']
 
-FloatVector: TypeAlias = AnyNDArray[_NumpyFloating] | Sequence[AnyFloat]
-FloatMatrix: TypeAlias = AnyNDArray[_NumpyFloating] | Sequence[FloatVector]
-FloatTensor: TypeAlias = AnyNDArray[_NumpyFloating] | Sequence['FloatTensor']
+_ArrayR: TypeAlias = AnyNDArray[_NpFloat] | _ArrayZ
+FloatVector: TypeAlias = _ArrayR | Sequence[AnyFloat]
+FloatMatrix: TypeAlias = _ArrayR | Sequence[Sequence[AnyFloat]]
+FloatTensor: TypeAlias = _ArrayR | Sequence[FloatMatrix | 'FloatTensor']
+
+_ArrayC: TypeAlias = AnyNDArray[_NpComplex] | _ArrayR
+ComplexVector: TypeAlias = _ArrayC | Sequence[AnyComplex]
+ComplexMatrix: TypeAlias = _ArrayC | Sequence[Sequence[AnyComplex]]
+ComplexTensor: TypeAlias = _ArrayC | Sequence[ComplexMatrix | 'ComplexTensor']
 
 # for numpy.sort
 SortKind: TypeAlias = Literal['quicksort', 'heapsort', 'stable']
 IndexOrder: TypeAlias = Literal['C', 'F', 'A', 'K']
+
+# numpy.polynomial
+
+@runtime_checkable
+class _SupportsCoef(Protocol):
+    coef: npt.NDArray[Any] | _SupportsArray[Any]
+
+
+@runtime_checkable
+class _SupportsDomain(Protocol):
+    domain: npt.NDArray[Any] | _SupportsArray[Any]
+
+
+@runtime_checkable
+class _SupportsWindow(Protocol):
+    window: npt.NDArray[Any] | _SupportsArray[Any]
+
+@runtime_checkable
+class _SupportsLessThanInt(Protocol):
+    def __lt__(self, __other: int) -> bool: ...
+
+_P = TypeVar('_P', bound='PolySeries')
+
+
+@runtime_checkable
+class PolySeries(Protocol):
+    """
+    Annotations for the (private) `numpy.polynomial._polybase.ABCPolyBase`
+    subtypes, e.g. [`numpy.polynomial.Legendre`][numpy.polynomial.Legendre].
+    """
+    __hash__: ClassVar[None]  # type: ignore[assignment]
+    __array_ufunc__: ClassVar[None]
+    maxpower: ClassVar[int]
+
+    basis_name: str | None
+
+    coef: npt.NDArray[_NpFloat | _NpComplex]
+    domain: npt.NDArray[_NpInt | _NpFloat | _NpComplex]
+    window: npt.NDArray[_NpInt | _NpFloat | _NpComplex]
+
+    @property
+    def symbol(self) -> str: ...
+
+    def has_samecoef(self, __other: _SupportsCoef) -> bool: ...
+    def has_samedomain(self, __other: _SupportsDomain) -> bool:...
+    def has_samewindow(self, __other: _SupportsWindow) -> bool: ...
+    def has_sametype(self: _P, __other: type[Any]) -> TypeGuard[type[_P]]: ...
+    def __init__(
+        self,
+        coef: npt.ArrayLike,
+        domain: ComplexVector | None = ...,
+        window: ComplexVector | None = ...,
+        symbol: str = ...,
+    ) -> None: ...
+    def __format__(self, __fmt_str: str) -> str: ...
+    @overload
+    def __call__(self, __arg: _P) -> _P: ...
+    @overload
+    def __call__(self, __arg: complex | _NpComplex) -> _NpComplex: ...
+    @overload
+    def __call__(self, __arg: AnyNumber) -> _NpFloat | _NpComplex: ...
+    @overload
+    def __call__(
+        self,
+        __arg: AnyNDArray[_NpNumber],
+    ) -> npt.NDArray[_NpFloat] | npt.NDArray[_NpComplex]: ...
+    def __iter__(self) -> Iterator[_NpFloat | _NpComplex]: ...
+    def __len__(self) -> int: ...
+    def __neg__(self: _P) -> _P: ...
+    def __pos__(self: _P) -> _P: ...
+    def __add__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __sub__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __mul__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __truediv__(self: _P, __other: AnyNumber) -> _P: ...
+    def __floordiv__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __mod__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __divmod__(self: _P, __other: npt.ArrayLike | _P) -> tuple[_P, _P]: ...
+    def __radd__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __rsub__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __rmul__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __rtruediv__(self: _P, __other: AnyNumber) -> _P: ...
+    def __rfloordiv__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __rmod__(self: _P, __other: npt.ArrayLike | _P) -> _P: ...
+    def __rdivmod__(
+        self: _P,
+        __other: npt.ArrayLike | _P,
+    ) -> tuple[_P, _P]: ...
+    def __eq__(self, __other: Any) -> bool: ...
+    def __ne__(self, __other: Any) -> bool: ...
+    def copy(self: _P) -> _P: ...
+    def degree(self) -> int: ...
+    def cutdeg(self: _P, deg: SupportsInt) -> _P: ...
+    def trim(self: _P, tol: AnyFloat | _SupportsLessThanInt = ...) -> _P: ...
+    def truncate(self: _P, size: AnyInt) -> _P: ...
+    @overload
+    def convert(
+        self,
+        domain: ComplexVector | None = ...,
+        *,
+        kind: type[_P],
+        window: ComplexVector = ...,
+    ) -> _P: ...
+    @overload
+    def convert(
+        self,
+        domain: ComplexVector,
+        kind: type[_P],
+        window: ComplexVector = ...,
+    ) -> _P: ...
+    @overload
+    def convert(
+        self: _P,
+        domain: ComplexVector = ...,
+        kind: type[_P] | None = ...,
+        window: ComplexVector = ...,
+    ) -> _P: ...
+    def mapparms(self) -> tuple[_NpFloat, _NpFloat]: ...
+    def integ(
+        self: _P,
+        m: AnyInt = ...,
+        k: npt.ArrayLike = ...,
+        lbnd: AnyNumber | None = ...,
+    ) -> _P: ...
+    def deriv(self: _P, m: AnyInt = ...) -> _P: ...
+    def roots(self) -> npt.NDArray[_NpFloat | _NpComplex]: ...
+    def linspace(
+        self,
+        n: AnyInt = ...,
+        domain: npt.ArrayLike | None = ...,
+    ) -> tuple[
+        npt.NDArray[_NpFloat | _NpComplex],
+        npt.NDArray[_NpFloat | _NpComplex],
+    ]: ...
+    @overload
+    @classmethod
+    def fit(
+        cls: type[_P],
+        x: npt.ArrayLike,
+        y: npt.ArrayLike,
+        deg: AnyInt | IntVector,
+        domain: ComplexVector | None = ...,
+        rcond: AnyFloat | None = ...,
+        *,
+        full: Literal[False],
+        w: FloatVector | None = ...,
+        window: ComplexVector | None = ...,
+        # symbol: str = ...,
+    ) -> _P: ...
+    @overload
+    @classmethod
+    def fit(
+        cls: type[_P],
+        x: npt.ArrayLike,
+        y: npt.ArrayLike,
+        deg: AnyInt | IntVector,
+        domain: ComplexVector | None = ...,
+        rcond: AnyFloat | None = ...,
+        *,
+        full: Literal[True],
+        w: FloatVector | None = ...,
+        window: ComplexVector | None = ...,
+        # symbol: str = ...,
+    ) -> tuple[_P, list[Any]]: ...
+    @overload
+    @classmethod
+    def fit(
+        cls: type[_P],
+        x: npt.ArrayLike,
+        y: npt.ArrayLike,
+        deg: AnyInt | IntVector,
+        domain: ComplexVector | None = ...,
+        rcond: AnyFloat | None = ...,
+        full: bool = ...,
+        w: FloatVector | None = ...,
+        window: ComplexVector | None = ...,
+        # symbol: str = ...,
+    ) -> _P: ...
+    @classmethod
+    def fromroots(
+        cls: type[_P],
+        roots: npt.ArrayLike,
+        domain: ComplexVector | None = ...,
+        window: ComplexVector | None = ...,
+        # symbol: str = ...,
+    ) -> _P: ...
+    @classmethod
+    def identity(
+        cls: type[_P],
+        domain: ComplexVector | None = ...,
+        window: ComplexVector | None = ...,
+        # symbol: str = ...,
+    ) -> _P: ...
+    @classmethod
+    def basis(
+        cls: type[_P],
+        deg: AnyInt,
+        domain: ComplexVector | None = ...,
+        window: ComplexVector | None = ...,
+        # symbol: str = ...,
+    ) -> _P: ...
+    @classmethod
+    def cast(
+        cls: type[_P],
+        series: 'PolySeries',
+        domain: ComplexVector | None = ...,
+        window: ComplexVector | None = ...,
+    ) -> _P: ...
+
 
 
 # PEP 692 precise **kwargs typing
@@ -83,3 +316,8 @@ class LMomentOptions(_LOptions, total=False):
 class LComomentOptions(_LOptions, total=False):
     """Use like `def spam(**kwargs: Unpack[LComomentOptions]): ...`."""
     rowvar: bool
+
+
+
+
+
