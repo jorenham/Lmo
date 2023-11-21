631208ff0ffc0b3e8b1cd4dea6eef5deb93528eb
jorenham <jhammudoglu@gmail.com>
2023-11-21 04:56:13 +0100

    Deployed b2bbb69 with MkDocs version: 1.5.3

--- scripts/mathjax.js
+++ scripts/mathjax.js
@@ -3,11 +3,35 @@ window.MathJax = {
         inlineMath: [["\\(", "\\)"]],
         displayMath: [["\\[", "\\]"]],
         processEscapes: true,
-        processEnvironments: true
+        processEnvironments: true,
+        tags: "ams",
+        macros: {
+            // Expectation operator
+            E: "\\mathop{\\mathbb{E}}",
+            // Beta function
+            B: "\\mathop{\\mathrm{B}}",
+            // Box-Cox transformation, and its inverse
+            boxcox: ["\\rm \\mathcal{P}_{#2}\\left( #1 \\right)", 2],
+            coxbox: ["\\rm \\mathcal{P}^{-1}_{#2}\\left( #1 \\right)", 2],
+            // Falling and rising factorials
+            ffact: ["\\left( #1 \\right)^{-}_{#2}", 2],
+            rfact: ["\\left( #1 \\right)^{+}_{#2}", 2],
+            // L-moment (untrimmed)
+            lmoment: ["\\lambda_{#1}", 1],
+            // L-moment (trimmed)
+            tlmoment: ["\\lambda^{(#1)}_{#2}", 2],
+            // L-moment ratio (untrimmed)
+            lratio: ["\\tau_{#1}", 1],
+            // L-moment ratio (trimmed)
+            tlratio: ["\\tau^{(#1)}_{#2}", 2]
+        }
     },
     options: {
-        ignoreHtmlClass: ".*|",
+        // ignoreHtmlClass: ".*",
         processHtmlClass: "arithmatex"
+    },
+    chtml: {
+        displayAlign: "left"
     }
 };
 
