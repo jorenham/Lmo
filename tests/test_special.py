7b59c451180a4f304dcad725734dc66455b7bd32
jorenham <jhammudoglu@gmail.com>
2023-12-18 15:24:44 +0100

    Fourier-Jacobi tests

--- tests/test_special.py
+++ tests/test_special.py
@@ -7,12 +7,11 @@ import numpy as np
 from numpy.polynomial.legendre import legval
 from numpy.testing import assert_allclose
 from lmo.special import fourier_jacobi
-# from scipy.special import eval_jacobi
+from scipy.special import eval_jacobi
 
 
 X = np.linspace(-1, 1, num=21, dtype=np.float64)
-
-@pytest.mark.parametrize('c', [
+C_EXAMPLES = [
     [0],
     [1],
     [-1],
@@ -24,10 +23,24 @@ X = np.linspace(-1, 1, num=21, dtype=np.float64)
     [0, 0, 1],
     [1, .5, .25],
     [512, 256, 128, 64, 32, 16, 8, 4, 2, 1],
-])
+]
+
+@pytest.mark.parametrize('c', C_EXAMPLES)
 def test_fourier_legendre(c: float | list[float]):
     y_expect = legval(X, c)
     y_true = fourier_jacobi(X, c, 0, 0)
 
     assert_allclose(y_true, y_expect)
 
+
+@pytest.mark.parametrize('a', [0, 1, 1/2, -1/2, 42])
+@pytest.mark.parametrize('b', [0, 1, 1/2, -1/2, 42])
+@pytest.mark.parametrize('c', C_EXAMPLES)
+def test_fourier_jacobi(a: float, b: float, c: float | list[float]):
+    y_expect = np.sum([
+        cn * eval_jacobi(n, a, b, X)
+        for n, cn in enumerate(c)
+    ], axis=0)
+    y_true = fourier_jacobi(X, c, a, b)
+
+    assert_allclose(y_true, y_expect, atol=1e-15)
