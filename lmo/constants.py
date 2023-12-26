76f175bb4bdd55fd8d3bf4d59cbe294145fa69e3
jorenham <jhammudoglu@gmail.com>
2023-12-26 21:55:21 +0100

    docstring cleanup in `constants`

--- lmo/constants.py
+++ lmo/constants.py
@@ -9,18 +9,18 @@ from typing import Final
 
 theta_m: Final[float] = 0.955316618124509278163857102515757754
 r"""
-Magic angle \( \theta_m = \arctan \sqrt 2 \approx 0.9553 \).
+Magic angle \( \theta_m = \arctan \sqrt 2 \).
 
 See also:
-    - https://wikipedia.org/wiki/Magic_angle
+    - [Magic angle - Wikipedia](https://wikipedia.org/wiki/Magic_angle)
 """
 
 theta_m_bar: Final[float] = 0.152043361992348182457286110194392272
 r"""
-Magic number of turns \( \bar{\theta}_m = \theta_m / (2 \pi) \approx 0.1520 \)
+Magic number of turns \( \bar{\theta}_m = \theta_m / (2 \pi) \).
 
 See also:
-    - [theta_m][lmo.constants.theta_m]
-    - https://wikipedia.org/wiki/Magic_angle
-    - https://wikipedia.org/wiki/Turn_(angle)
+    - [`lmo.constants.theta_m`][lmo.constants.theta_m]
+    - [Magic angle - Wikipedia](https://wikipedia.org/wiki/Magic_angle)
+    - [Turn (angle) - Wikipedia](https://wikipedia.org/wiki/Turn_(angle))
 """
