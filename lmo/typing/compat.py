e0a15879325426bd2197e9b23f617806d86cbd85
jorenham <jhammudoglu@gmail.com>
2024-06-25 05:39:53 +0200

    extend `lmo.typing.compat`

--- lmo/typing/compat.py
+++ lmo/typing/compat.py
@@ -1,38 +1,47 @@
-"""Typing compatibility for Python <3.11."""
+"""Typing compatibility for Python <3.13."""
 import sys
 
 
-if sys.version_info < (3, 13):
-    from typing_extensions import (
-        LiteralString,
-        ParamSpec,
-        Self,
-        TypeVar,
-        TypeVarTuple,
-        Unpack,
-        assert_never,
-        assert_type,
-    )
-else:
+if sys.version_info >= (3, 13):
     from typing import (
         LiteralString,
         ParamSpec,
+        Protocol,
         Self,
         TypeVar,
         TypeVarTuple,
         Unpack,
         assert_never,
         assert_type,
+        overload,
+        runtime_checkable,
+    )
+else:
+    from typing_extensions import (
+        LiteralString,
+        ParamSpec,
+        Protocol,
+        Self,
+        TypeVar,
+        TypeVarTuple,
+        Unpack,
+        assert_never,
+        assert_type,
+        overload,
+        runtime_checkable,
     )
 
 
 __all__ = (
     'LiteralString',
     'ParamSpec',
+    'Protocol',
     'Self',
     'TypeVar',
     'TypeVarTuple',
     'Unpack',
     'assert_never',
     'assert_type',
+    'overload',
+    'runtime_checkable',
 )
